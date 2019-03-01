182604956a6d96281cd158c3f8ce48cbb092af2c
Josh Nederveld <joshnederveld@gmail.com>
2019-03-01 10:27:00 -0500

    Update version number

--- acf-flex-layouts.php
+++ acf-flex-layouts.php
@@ -1,22 +1,11 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
- * @since             1.0.0
- * @package           ACF_Flex_Layouts
- *
  * @wordpress-plugin
  * Plugin Name:       ACF Flex Layouts
  * Plugin URI:        https://github.com/Clark-Nikdel-Powell/ACF-Flex-Layouts/
  * Description:       A library of pre-built flexible content layouts for ACF.
- * Version:           1.3.0
+ * Version:           1.4.0
  * Author:            CNP
  * Author URI:        http://cnpagency.com/
  * License:           GPL-2.0+
